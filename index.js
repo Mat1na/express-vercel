43cfe36de3edf2687de367115ebf68ee22b86b34
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-09-28 19:41:57 +0200

    updated

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const product = require("./api/product");
-
+const root = require('./api/root')
 app.use(express.json({ extended: false }));
 
-app.use("/api/product", product);
 
+app.use("/api/product", product);
+app.use("/",root)
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
