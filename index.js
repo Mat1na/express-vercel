3ac2571094612e2c2070420157c5adcfe270bd18
5tamatia <Stamatia.Bampa@Student.intecbrussel.be>
2022-10-03 14:39:23 +0200

    updated

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require("express");
 const app = express();
 const product = require("./api/product");
 const root = require('./api/root')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 app.use(express.json({ extended: false }));
 
 
-app.use("/api/product", product);
-app.use("/",root)
+//proxy server
+app.use(
+    '/news', createProxyMiddleware({
+      target :'https://newsapi.org/v2' ,
+      pathRewrite:{'/news':''}, // http://localhost:4000/news
+      changeOrigin:true,
+      secure:true
+    })
+)
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
